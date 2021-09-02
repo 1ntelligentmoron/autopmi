628586b4d9d2fec655e0e7c563e8e9c7081bb8e6
1ntelligentmoron <74859246+1ntelligentmoron@users.noreply.github.com>
2021-09-02 22:58:03 +0800

    Commit #4: Functionality developemnt complete (that's quick)

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # 1ntelligentMoron's AutoPMI
 
 # What is AutoPMI?
-AutoPMI is a tool for 21-22 MS3V students to join afternoon Zoom lessons automatically, discarding the need to look up which teacher's class to join, finding the teacher's PMI, and entering the PMI in Zoom. This app is designed for Windows 10 users ONLY, with MacOS and Linux support coming much later (perhaps until the features are fully fletched out). If you have any issues using or setting up the program, feel free to DM me in WhatsApp, Discord (1ntelligentMoron#8260), or Telegram. 
+AutoPMI is a tool for Logos IB M23 Cohort (i.e. 21-22 MS3) students to join afternoon Zoom lessons automatically, discarding the need to look up which teacher's class to join, finding the teacher's PMI, and entering the PMI in Zoom. This app is designed for Windows 10 ONLY, with MacOS and Linux support coming much later (perhaps until the features are fully fletched out). If you have any issues using or setting up the program, feel free to DM me in WhatsApp, Discord, or Telegram. Figure how to find me yourself.
 
 Bear in mind that it is your responsibility to ensure that you attend the correct lesson on time, this tool is to simplify the process of which. If you are late because of this tool not working as intended, I cannot do anything to help you.
 
--- pmi.py
+++ pmi.py
@@ -1,23 +1,19 @@
-import webbrowser
+# Import required libraries
+import webbrowser as web
 import pyautogui as gui
 import time
+from datetime import datetime
+dt = datetime.now()
 
 
-# QoL functions
-def get_val(Nx2_2d_list, key):
-    for kv_pair in Nx2_2d_list:
-        if kv_pair[0] == key:
-            return kv_pair[1]
-
-
-# Read config
+# Read config.txt
 with open('config.txt', 'r') as file:
     configs = file.readlines()
 
 option = []
 pmi = []
 sched = []
-line_no = 0
+line_no = 0  # temp index for assigning key-value pairs to lists
 
 for config in configs:
     
@@ -32,25 +28,53 @@ for config in configs:
         option.append(kv_pair)
     elif 15 <= line_no <= 34:
         pmi.append(kv_pair)
-    elif 43 <= line_no <= 82:
+    elif 45 <= line_no <= 83:
         sched.append(kv_pair)
-        
-# print("option", option)
-# print("pmi", pmi)
-# print("sched", sched)
 
+    
+# QoL functions
+def get_val(Nx2_2d_list, key):
+    for kv_pair in Nx2_2d_list:
+        if kv_pair[0] == key:
+            return kv_pair[1]
+    return
 
-def join_zoom(subject):
-    webbrowser.open('https://zoom.us/j/')
-    time.sleep(3)
+def join_zoom(subject: str):
+    url = f'https://zoom.us/j/{get_val(pmi, subject)}'
+    web.open(url)
+    time.sleep(int(get_val(option, 'wait')))  # wait for browser to load page
     gui.press('left')
     gui.press('enter')
 
 
 # Script
 def main():
-    join_zoom(None)
+    
+    while True:
+        
+        week = input('Week A or B? ').upper()  # Week ident
+        if week not in ('A', 'B'):  # Week validation
+            print('Invalid week, try again.\n')
+            continue
+        day = dt.strftime('%w')  # Day of week
+        hhmm = int(dt.strftime('%H') + dt.strftime('%M'))  # Time in str(HHMM)
+        
+        if day in ('0', '6') or not 1540 <= hhmm <= 1745:  # Exit if weekend or no Zoom lesson coming up
+            print('No upcoming lesson.')
+            break
+        
+        if 1540 <= hhmm < 1620:  # Get lesson number
+            lesson_no = '8'
+        elif 1620 <= hhmm < 1655:
+            lesson_no = '9'
+        else:
+            lesson_no = '10'
+            
+        lesson_ident = f'{week}{day}_lesson_{lesson_no}'  # Construct key for 'sched'
+        upcoming_subj = get_val(sched, lesson_ident)  # Get subject name
+        join_zoom(upcoming_subj)  # Join Zoom
+        break
     
     
 if __name__ == "__main__":
-    pass  # main()
+    main()
